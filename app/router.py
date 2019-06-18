48d0f65529eeaaefbeeb383d85fe395836207ef0
SohnYongJin <winiel@smfcast.com>
2019-06-19 01:14:18 +0900

    init

---
+++ app/router.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+
+# from application.controller.push_mail import PushMail
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    print("flask_sample");
+    return render_template('home.html')
+# def hello_world():
+#     return 'Hello World!'
+
+
+
+@app.route('/test')
+def test():
+    # dbUtil = DbUtil();
+    # dbUtil = DbUtil();
+    #
+    # sql = "SELECT * FROM abitree.tbl_test";
+    # res = dbUtil.exeQuery(sql, None );
+    # print(res);
+    return "hello12";
+
+
+
+
+@app.route('/mail', methods=['post'])
+def PostMail():
+
+    # pushMail = PushMail();
+    # pushMail.push(request);
+
+    return "post_mail22";
+
+
+if __name__ == '__main__':
+    app.run()
